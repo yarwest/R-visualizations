e8486d97fdffca5b29ef15844fb6bf8356431b50
Yarno Boelens <yarno@print.com>
2020-03-20 16:47:23 +0100

    Line charts: tried adding labels to vertical lines and general improvement

--- SI Graphs.R
+++ SI Graphs.R
@@ -37,7 +37,7 @@ generateLabels <- function(yearsParam, nDataPoints) {
 # Optional label for y axis
 # Pass a vector of vertical line points
 # Optional restrictions for the y axis
-timeLinePlot <- function(data, yearsParam, titleParam, xLabel="", yLabel="", verticalLines, y=c(NA,NA)) {
+timeLinePlot <- function(data, yearsParam, titleParam, xLabel="", yLabel="", yAxisLabels="", verticalLines, y=c(NA,NA)) {
   nDataSets <- length(data)
   nDataPoints <- length(data[[1]])
   labels <- generateLabels(yearsParam, nDataPoints)
@@ -50,21 +50,26 @@ timeLinePlot <- function(data, yearsParam, titleParam, xLabel="", yLabel="", ver
   
   colSeq <- c(rep(colors[1],nDataPoints), rep(colors[2],nDataPoints))
   
-  drawVerticalLines <- function(x) {
-    geom_vline(xintercept = x, lty=3, lwd=1)
+  avgY <- mean(df$y)
+  drawVerticalLines <- function(line) {
+    geom_text(aes(x=line[1], label=paste("\n",line[2]), y=avgY), angle=90, color="black")
+    geom_vline(xintercept = line[1], lty=3, lwd=1)
   }
+
   lapply(X=verticalLines, FUN=drawVerticalLines)
   
   ggplot(df, aes(x=x, y=y, group=country, color=colSeq)) +
     geom_line(size=1) +
     geom_point(size=2) +
-    #geom_hline(yintercept = 0) +
     geom_vline(xintercept = 4, col="Black", lty=3, lwd=2) +
+    geom_text(aes(x=4, label="\nRecession", y=avgY), angle=90, color="black") +
     lapply(X=verticalLines, FUN=drawVerticalLines) +
     ggtitle(titleParam) +
     labs(x=xLabel, y=yLabel) +
-    theme(text = element_text(size=15)) +
-    scale_y_continuous(limits = y) +
+    theme(text = element_text(size=25)) +
+    theme_light() +
+    scale_y_continuous(breaks=c(yAxisLabels$y), labels=yAxisLabels$label) +
+    coord_cartesian(ylim = y) +
     scale_colour_manual(name="Countries",
                         breaks=colors,
                         values=c("#f7bb3d"=colors[1], "#cd5038"=colors[2]),
@@ -84,7 +89,14 @@ timeLinePlot(
   titleParam="Comparison Satisfaction National Economy Poland and Hungary",
   xLabel = "Years",
   yLabel = "Satisfaction with National Economy (in %)",
-  c(5, 7.5),
+  yAxisLabels = c(
+    list(y=0,label="0"),
+    list(y=25,label="25"),
+    list(y=50,label="50"),
+    list(y=75,label="75"),
+    list(y=100,label="100")
+  ),
+  c(list(x=5,label="Fidesz won election"), list(x=7.5,label="PiS won election")),
   y=c(0,100))
 
 ######################
@@ -93,6 +105,7 @@ timeLinePlot(
 
 meanSatisfactionEconomyHUFidesz <- c(3.96, 2.77, 2.14, 1.44, 3.48, 3.87, 4.91, 5.99)
 meanSatisfactionEconomyPLPiS <- c(2.76, 3.47, 4.3, 4.03, 3.85, 3.44, 3.56, 5.5)
+possibleAnswers <- c("Extremely dissatisfied", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Extremely satisfied")
 
 timeLinePlot(
   list(meanSatisfactionEconomyHUFidesz, meanSatisfactionEconomyPLPiS),
@@ -100,7 +113,9 @@ timeLinePlot(
   titleParam="Satisfaction National Economy PiS and Fidesz",
   xLabel = "Years",
   yLabel = "Satisfaction (Mean Value)",
-  c(5, 7.5))
+  yAxisLabels = possibleAnswers,
+  c(list(x=5,label="Fidesz won election"), list(x=7.5,label="PiS won election")),
+  y=c(0,10))
 
 ######################
 # Mean Comparison Satisfaction National Economy Poland and Hungary_Total N
@@ -108,7 +123,7 @@ timeLinePlot(
 
 meanSatisfactionEconomyHU <- c(4.09, 3.12, 2.89, 1.82, 3.16, 3.35, 3.79, 4.82)
 meanSatisfactionEconomyPL <- c(2.76, 3.19, 4.08, 4.45, 4.45, 4.2, 4.08, 4.86)
-
+possibleAnswers <- c("Extremely dissatisfied", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Extremely satisfied")
 
 timeLinePlot(
   list(meanSatisfactionEconomyHU, meanSatisfactionEconomyPL),
@@ -116,7 +131,9 @@ timeLinePlot(
   titleParam="Satisfaction National Economy Poland and Hungary",
   xLabel = "Years",
   yLabel = "Satisfaction (Mean Value)",
-  c(5, 7.5))
+  yAxisLabels = possibleAnswers,
+  c(list(x=5,label="Fidesz won election"), list(x=7.5,label="PiS won election")),
+  y=c(0,10))
 
 ######################
 # Comparison Feeling About Household Income Nowadays Poland and Hungary
@@ -131,5 +148,12 @@ timeLinePlot(
   titleParam="Comparison Feeling About Household Income Nowadays Poland and Hungary",
   xLabel = "Years",
   yLabel = "Feeling About Household Income (in %)",
-  c(5, 7.5),
+  yAxisLabels = c(
+    list(y=0,label="0"),
+    list(y=25,label="25"),
+    list(y=50,label="50"),
+    list(y=75,label="75"),
+    list(y=100,label="100")
+  ),
+  c(list(x=5,label="Fidesz won election"), list(x=7.5,label="PiS won election")),
   y=c(0,100))
