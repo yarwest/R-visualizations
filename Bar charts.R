bf8e78f9a490325bbff656f71b38aa5695ab8ced
Yarno Boelens <yarno@print.com>
2020-03-25 22:32:49 +0100

    Updated Bar Charts styling

--- Bar charts.R
+++ Bar charts.R
@@ -47,10 +47,9 @@ barPlot <- function(labels, groupLabels, yLabels, data, y) {
     scale_x_continuous(breaks=xGroups, labels=groupLabels) +
     scale_y_continuous(breaks=c(y[1]:y[2]), labels=yLabels) +
     coord_cartesian(ylim = y) +
-    theme_light() +
+    theme_linedraw() +
     theme(
-      text = element_text(size=17, color="#4C4D4D"),
-      plot.title=element_text(hjust = 0.5),
+      text = element_text(size=16, color="Black"),
       legend.position=c(0.9,0.8)
     ) +
     scale_fill_manual(values=colors)
@@ -69,7 +68,7 @@ barPlot(labels, questions, possibleAnswers, data, y=c(1,11))
 
 ## question 2 & 3
 labels <- rep(c(2008, 2016), 4)
-questions <- c("How likely unemployed and looking for work next 12 months?", "How likely not enough money for household \n necessities next 12 months?")
+questions <- c("How likely unemployed/nand looking for work next 12 months?", "How likely not enough money for household \n necessities next 12 months?")
 possibleAnswers <- c("Not likely at all", "Not very likely", "Likely", "Very likely")
 
 data <- c(2.25, 2.33, 1.93, 1.58, 1.38, 1.07, 1.36, 0.994)
